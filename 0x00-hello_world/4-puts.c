287a7db43ebdf91f67981175ef78b29bc4f43682
XimbaJ <mtshali0301@gmail.com>
2023-03-17 03:10:25 -0700

    program that prints exactly text using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program will print text using puts function
+ * Return: value 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
